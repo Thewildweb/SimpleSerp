39d6aa51b77ebc951bc39750d59160d52430e358
Thewildweb <erik@thewildweb.nl>
2021-03-22 10:52:50 +0000

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='SimpleSerp',
-    version='0.01',
-    description='Library for scraping Google SERP',
-    author='Erik Meijer',
-    author_email='erik@datadepartment.io',
-    url='https://github.com/Thewildweb/SimpleSerp.git',
-    packages=['playwright', 'pydantic', 'selectolax'],
+    name="SimpleSerp",
+    version="0.01",
+    description="Library for scraping Google SERP",
+    author="Erik Meijer",
+    author_email="erik@datadepartment.io",
+    url="https://github.com/Thewildweb/SimpleSerp.git",
+    packages=["simple_serp"],
+    install_requires=["playwright", "pydantic", "selectolax"],
 )
